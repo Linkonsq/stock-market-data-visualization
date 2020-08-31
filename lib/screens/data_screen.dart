c025934ccc7cd1d495da409587b9e1c34562a9a0
Linkon Siddique <linkon.sq48@gmail.com>
2020-09-01 00:24:15 +0600

    Make table scrollable horizontally

--- lib/screens/data_screen.dart
+++ lib/screens/data_screen.dart
@@ -41,12 +41,6 @@ class _DataScreenState extends State<DataScreen> {
       appBar: AppBar(
         title: Text('Stock Market Data Visualization'),
       ),
-//      body: SingleChildScrollView(
-//        scrollDirection: Axis.horizontal,
-//        child: SingleChildScrollView(
-//          child: dataBody(data),
-//        ),
-//      ),
       body: _isLoading
           ? Center(
               child: CircularProgressIndicator(),
@@ -54,73 +48,76 @@ class _DataScreenState extends State<DataScreen> {
           : ListView.builder(
               itemCount: data.length,
               itemBuilder: (ctx, index) {
-                return DataTable(
-                  columns: [
-                    DataColumn(
-                      label: Text('Date'),
-                      numeric: false,
-                      tooltip: 'This is Date',
-                    ),
-                    DataColumn(
-                      label: Text('Trade Code'),
-                      numeric: false,
-                      tooltip: 'This is Trade Code',
-                    ),
-                    DataColumn(
-                      label: Text('High'),
-                      numeric: false,
-                      tooltip: 'This is High',
-                    ),
-                    DataColumn(
-                      label: Text('Low'),
-                      numeric: false,
-                      tooltip: 'This is Low',
-                    ),
-                    DataColumn(
-                      label: Text('Open'),
-                      numeric: false,
-                      tooltip: 'This is Open',
-                    ),
-                    DataColumn(
-                      label: Text('Close'),
-                      numeric: false,
-                      tooltip: 'This is Close',
-                    ),
-                    DataColumn(
-                      label: Text('Volume'),
-                      numeric: false,
-                      tooltip: 'This is Volume',
-                    ),
-                  ],
-                  rows: data
-                      .map(
-                        (dt) => DataRow(
-                          cells: [
-                            DataCell(
-                              Text(dt.date),
-                            ),
-                            DataCell(
-                              Text(dt.tradeCode),
-                            ),
-                            DataCell(
-                              Text(dt.high),
-                            ),
-                            DataCell(
-                              Text(dt.low),
-                            ),
-                            DataCell(
-                              Text(dt.open),
-                            ),
-                            DataCell(
-                              Text(dt.close),
-                            ),
-                            DataCell(
-                              Text(dt.volume),
-                            ),
-                          ],
-                        ),
-                      )
-                      .toList(),
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columns: [
+                      DataColumn(
+                        label: Text('Date'),
+                        numeric: false,
+                        tooltip: 'This is Date',
+                      ),
+                      DataColumn(
+                        label: Text('Trade Code'),
+                        numeric: false,
+                        tooltip: 'This is Trade Code',
+                      ),
+                      DataColumn(
+                        label: Text('High'),
+                        numeric: false,
+                        tooltip: 'This is High',
+                      ),
+                      DataColumn(
+                        label: Text('Low'),
+                        numeric: false,
+                        tooltip: 'This is Low',
+                      ),
+                      DataColumn(
+                        label: Text('Open'),
+                        numeric: false,
+                        tooltip: 'This is Open',
+                      ),
+                      DataColumn(
+                        label: Text('Close'),
+                        numeric: false,
+                        tooltip: 'This is Close',
+                      ),
+                      DataColumn(
+                        label: Text('Volume'),
+                        numeric: false,
+                        tooltip: 'This is Volume',
+                      ),
+                    ],
+                    rows: data
+                        .map(
+                          (dt) => DataRow(
+                            cells: [
+                              DataCell(
+                                Text(dt.date),
+                              ),
+                              DataCell(
+                                Text(dt.tradeCode),
+                              ),
+                              DataCell(
+                                Text(dt.high),
+                              ),
+                              DataCell(
+                                Text(dt.low),
+                              ),
+                              DataCell(
+                                Text(dt.open),
+                              ),
+                              DataCell(
+                                Text(dt.close),
+                              ),
+                              DataCell(
+                                Text(dt.volume),
+                              ),
+                            ],
+                          ),
+                        )
+                        .toList(),
+                  ),
                 );
               },
             ),
