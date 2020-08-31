04baec447309a7bb98977bf330b112115ca7cb6e
Linkon Siddique <linkon.sq48@gmail.com>
2020-09-01 00:18:51 +0600

    Showing all data from server

--- lib/screens/data_screen.dart
+++ lib/screens/data_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stock_market_data/providers/data.dart';
 import 'package:stock_market_data/providers/data_list.dart';
 
 class DataScreen extends StatefulWidget {
@@ -10,7 +9,7 @@ class DataScreen extends StatefulWidget {
 
 class _DataScreenState extends State<DataScreen> {
   var _isInit = true;
-  //var _isLoading = false;
+  var _isLoading = false;
 
   @override
   void initState() {
@@ -20,97 +19,17 @@ class _DataScreenState extends State<DataScreen> {
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      Provider.of<DataList>(context).fetchData();
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<DataList>(context).fetchData().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
     }
     _isInit = false;
     super.didChangeDependencies();
-  }
-
-//  @override
-//  void didChangeDependencies() {
-//    if (_isInit) {
-//      setState(() {
-//        _isLoading = true;
-//      });
-//      Provider.of<DataList>(context).fetchData().then((_) {
-//        setState(() {
-//          _isLoading = false;
-//        });
-//      });
-//    }
-//    _isInit = false;
-//    super.didChangeDependencies();
-//  }
-
-  DataTable dataBody(List<Data> dataInfo) {
-    return DataTable(
-      columns: [
-        DataColumn(
-          label: Text('Date'),
-          numeric: false,
-          tooltip: 'This is Date',
-        ),
-        DataColumn(
-          label: Text('Trade Code'),
-          numeric: false,
-          tooltip: 'This is Trade Code',
-        ),
-        DataColumn(
-          label: Text('High'),
-          numeric: false,
-          tooltip: 'This is High',
-        ),
-        DataColumn(
-          label: Text('Low'),
-          numeric: false,
-          tooltip: 'This is Low',
-        ),
-        DataColumn(
-          label: Text('Open'),
-          numeric: false,
-          tooltip: 'This is Open',
-        ),
-        DataColumn(
-          label: Text('Close'),
-          numeric: false,
-          tooltip: 'This is Close',
-        ),
-        DataColumn(
-          label: Text('Volume'),
-          numeric: false,
-          tooltip: 'This is Volume',
-        ),
-      ],
-      rows: dataInfo
-          .map(
-            (data) => DataRow(
-              cells: [
-                DataCell(
-                  Text(data.date),
-                ),
-                DataCell(
-                  Text(data.tradeCode),
-                ),
-                DataCell(
-                  Text(data.high),
-                ),
-                DataCell(
-                  Text(data.low),
-                ),
-                DataCell(
-                  Text(data.open),
-                ),
-                DataCell(
-                  Text(data.close),
-                ),
-                DataCell(
-                  Text(data.volume),
-                ),
-              ],
-            ),
-          )
-          .toList(),
-    );
   }
 
   @override
@@ -122,33 +41,89 @@ class _DataScreenState extends State<DataScreen> {
       appBar: AppBar(
         title: Text('Stock Market Data Visualization'),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: SingleChildScrollView(
-          child: dataBody(data),
-        ),
-      ),
+//      body: SingleChildScrollView(
+//        scrollDirection: Axis.horizontal,
+//        child: SingleChildScrollView(
+//          child: dataBody(data),
+//        ),
+//      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (ctx, index) {
+                return DataTable(
+                  columns: [
+                    DataColumn(
+                      label: Text('Date'),
+                      numeric: false,
+                      tooltip: 'This is Date',
+                    ),
+                    DataColumn(
+                      label: Text('Trade Code'),
+                      numeric: false,
+                      tooltip: 'This is Trade Code',
+                    ),
+                    DataColumn(
+                      label: Text('High'),
+                      numeric: false,
+                      tooltip: 'This is High',
+                    ),
+                    DataColumn(
+                      label: Text('Low'),
+                      numeric: false,
+                      tooltip: 'This is Low',
+                    ),
+                    DataColumn(
+                      label: Text('Open'),
+                      numeric: false,
+                      tooltip: 'This is Open',
+                    ),
+                    DataColumn(
+                      label: Text('Close'),
+                      numeric: false,
+                      tooltip: 'This is Close',
+                    ),
+                    DataColumn(
+                      label: Text('Volume'),
+                      numeric: false,
+                      tooltip: 'This is Volume',
+                    ),
+                  ],
+                  rows: data
+                      .map(
+                        (dt) => DataRow(
+                          cells: [
+                            DataCell(
+                              Text(dt.date),
+                            ),
+                            DataCell(
+                              Text(dt.tradeCode),
+                            ),
+                            DataCell(
+                              Text(dt.high),
+                            ),
+                            DataCell(
+                              Text(dt.low),
+                            ),
+                            DataCell(
+                              Text(dt.open),
+                            ),
+                            DataCell(
+                              Text(dt.close),
+                            ),
+                            DataCell(
+                              Text(dt.volume),
+                            ),
+                          ],
+                        ),
+                      )
+                      .toList(),
+                );
+              },
+            ),
     );
-
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text('Stock Market Data Visualization'),
-//      ),
-//      body: //_isLoading
-////          ? Center(
-////              child: CircularProgressIndicator(),
-////            )
-//          ListView(
-//        children: <Widget>[
-//          Column(
-//            children: <Widget>[
-//              Center(
-//                child: dataBody(data),
-//              )
-//            ],
-//          )
-//        ],
-//      ),
-//    );
   }
 }
