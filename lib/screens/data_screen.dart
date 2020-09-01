d4bfe15135fb5bd2773e377c431285f2da3625fc
Linkon Siddique <linkon.sq48@gmail.com>
2020-09-01 23:21:52 +0600

    Check for internet connection

--- lib/screens/data_screen.dart
+++ lib/screens/data_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stock_market_data/providers/data_list.dart';
+import 'package:connectivity/connectivity.dart';
 
 class DataScreen extends StatefulWidget {
   @override
@@ -10,9 +11,11 @@ class DataScreen extends StatefulWidget {
 class _DataScreenState extends State<DataScreen> {
   var _isInit = true;
   var _isLoading = false;
+  bool _isInternetOn = true;
 
   @override
   void initState() {
+    connectionCheck();
     super.initState();
   }
 
@@ -32,6 +35,15 @@ class _DataScreenState extends State<DataScreen> {
     super.didChangeDependencies();
   }
 
+  Future<void> connectionCheck() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _isInternetOn = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final dataInfo = Provider.of<DataList>(context);
@@ -41,45 +53,54 @@ class _DataScreenState extends State<DataScreen> {
       appBar: AppBar(
         title: Text('Stock Market Data Visualization'),
       ),
-      body: _isLoading
+      body: !_isInternetOn
           ? Center(
-              child: CircularProgressIndicator(),
+              child: Text(
+                'Please connect to the internet and re-open the app.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             )
-          : ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (ctx, index) {
-                return SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: DataTable(
-                    columns: [
-                      buildDataColumn('Date', false, 'This is Date'),
-                      buildDataColumn(
-                          'Trade Code', false, 'This is Trade Code'),
-                      buildDataColumn('High', false, 'This is High'),
-                      buildDataColumn('Low', false, 'This is Low'),
-                      buildDataColumn('Open', false, 'This is Open'),
-                      buildDataColumn('Close', false, 'This is Close'),
-                      buildDataColumn('Volume', false, 'This is Volume'),
-                    ],
-                    rows: data
-                        .map(
-                          (dt) => DataRow(
-                            cells: [
-                              buildDataCell(dt.date),
-                              buildDataCell(dt.tradeCode),
-                              buildDataCell(dt.high),
-                              buildDataCell(dt.low),
-                              buildDataCell(dt.open),
-                              buildDataCell(dt.close),
-                              buildDataCell(dt.volume),
-                            ],
-                          ),
-                        )
-                        .toList(),
-                  ),
-                );
-              },
-            ),
+          : _isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (ctx, index) {
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTable(
+                        columns: [
+                          buildDataColumn('Date', false, 'This is Date'),
+                          buildDataColumn(
+                              'Trade Code', false, 'This is Trade Code'),
+                          buildDataColumn('High', false, 'This is High'),
+                          buildDataColumn('Low', false, 'This is Low'),
+                          buildDataColumn('Open', false, 'This is Open'),
+                          buildDataColumn('Close', false, 'This is Close'),
+                          buildDataColumn('Volume', false, 'This is Volume'),
+                        ],
+                        rows: data
+                            .map(
+                              (dt) => DataRow(
+                                cells: [
+                                  buildDataCell(dt.date),
+                                  buildDataCell(dt.tradeCode),
+                                  buildDataCell(dt.high),
+                                  buildDataCell(dt.low),
+                                  buildDataCell(dt.open),
+                                  buildDataCell(dt.close),
+                                  buildDataCell(dt.volume),
+                                ],
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    );
+                  },
+                ),
     );
   }
 
